6349ddf9b7bf717ed444eddea2373587f97183a9
Matt Quinn <mjq0533@gmail.com>
2022-02-25 18:31:41 -0600

    commit tree a4e4e6a44b19ab75c2ec1dbbb9e2d39fb72b8d72

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
 
+class CashRegister
+
+    attr_accessor :total, :discount, :items
+
+  def  initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+
+  end
+
+  def add_item(title, price, quantity = 1)
+
+  end
+
+end
